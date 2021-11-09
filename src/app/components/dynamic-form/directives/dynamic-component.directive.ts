fc3fca8e949ebaef3de54fe240603a15f6c15e32
cunundrummer <cunundrummer@gmail.com>
2021-11-09 10:26:44 -0500

    - Marked config, and group as optional since they are not being immediately used.  Likewise, elRef was removed since it was not used and was causing a warning.

--- src/app/components/dynamic-form/directives/dynamic-component.directive.ts
+++ src/app/components/dynamic-form/directives/dynamic-component.directive.ts
@@ -1,14 +1,14 @@
-import { Directive, ElementRef, Input, OnInit, ViewContainerRef } from '@angular/core';
+import { Directive, Input, OnInit, ViewContainerRef } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
 @Directive({
   selector: '[dynamicComponent]'
 })
 export class DynamicComponentDirective implements OnInit{
-  @Input() config: any; // will not receive anything unless PASSED AS [input]='xx'
-  @Input() group!: FormGroup; // will not receive anything unless PASSED AS [input]='xx'
+  @Input() config?: any; // will not receive anything unless PASSED AS [input]='xx'
+  @Input() group?: FormGroup; // will not receive anything unless PASSED AS [input]='xx'
 
-  constructor(public viewContainerRef: ViewContainerRef, private elRef: ElementRef) {
+  constructor(public viewContainerRef: ViewContainerRef) {
     /**
      *  DynamicComponentDirective injects ViewContainerRef to gain access to the view container of the element that will
      *  host the dynamically added component.
